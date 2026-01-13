1c3294718f32c8e7431fc85102179ff5c6cad56c
Anabella <102830512+anabanana93@users.noreply.github.com>
2026-01-13 16:13:09 +0000

    Update Example1.java

--- src/Example1.java
+++ src/Example1.java
@@ -2,6 +2,11 @@ public class Example1 {
     public static void main(String... args) {
         Storage<BankAccount> bankAccountStorage = new Storage<BankAccount>();
 Storage<String> stringStorage = new Storage<String>();
+        Object account = new BankAccount(2025);
+bankAccountStorage.setItem(account);
+
+Object account1 = bankAccountStorage.getItem();
+account1.deposit(15);
         
     }
 }
